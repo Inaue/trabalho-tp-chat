777b625e455e74b77dc37953e1baaaa02952d558
Inaue Ferreira da Silva <inaue.silva@gmail.com>
2023-03-26 22:59:16 -0300

    Multiple clients and server shutdown alone.

--- Servidor_Chat.java
+++ Servidor_Chat.java
@@ -1,25 +1,95 @@
 import java.net.*;
+import java.lang.Thread;
 import java.io.IOException;
 import java.util.Scanner;
 
 class Servidor_Chat
 {
-	public static void main(String[] args) throws IOException
+	public static void main(String[] args)
+			throws IOException, InterruptedException
 	{
-		int porta		= 4381;
-		ServerSocket servidor	= new ServerSocket(porta);
-		Socket cliente		= servidor.accept();
-        	Scanner msg		= new Scanner(cliente.getInputStream());
+		int porta			= 4381;
+		ServerSocket servidor_chat	= new ServerSocket(porta);
+		Gestor_Conexoes gestao_chat	= new Gestor_Conexoes(servidor_chat);
 
+		servidor_chat.setSoTimeout(5 * 1000);
 		System.out.println("App de Conversacoes");
 		System.out.println("______________________________________________");
-		System.out.println("Conectado com usuario " + cliente.getInetAddress().getHostAddress());
+		gestao_chat.start();
 
-		while(msg.hasNextLine())
-			System.out.println(msg.nextLine());
+		do
+		{
+			Thread.sleep(3 * 1000);
+		}
+		while(Conexao.activeCount() != 1);
 
-		msg.close();
-		cliente.close();
-		servidor.close();
+		System.out.println("______________________________________________");
+		System.out.println("Fim do Bate-papo!");
+
+		servidor_chat.close();
+	}
+}
+
+class Gestor_Conexoes extends Thread
+{
+	private ServerSocket server;
+
+	public Gestor_Conexoes(ServerSocket server)
+	{
+		this.server	= server;
+	}
+
+	public void run()
+	{
+		while(Conexao.activeCount() < 3)
+		{
+			try
+			{
+				new Conexao(this.server.accept());
+			}
+			catch (Exception e) {}
+		}
+
+		while(Conexao.activeCount() != 2)
+		{
+			try
+			{
+					new Conexao(this.server.accept());
+			}
+			catch (Exception e) {}
+		}
+	}
+}
+
+class Conexao extends Thread
+{
+	private Socket cliente;
+
+	public Conexao(Socket endereco_cliente)
+	{
+		this.cliente = endereco_cliente;
+		this.start();
+	}
+
+	public void run()
+	{
+		try
+		{
+			Scanner msg = new Scanner(this.cliente.getInputStream());
+			String ip_cliente = this.cliente.getInetAddress().getHostAddress();
+
+			System.out.println(ip_cliente + " se conectou.");
+
+			while(msg.hasNextLine())
+				System.out.println(msg.nextLine());
+
+			System.out.println(ip_cliente + " se desconectou.");
+			msg.close();
+			this.cliente.close();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
