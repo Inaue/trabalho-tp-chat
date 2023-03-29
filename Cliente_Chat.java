1b5c4fea8c4e118cb246edd6ce70f82a3f816d40
Inaue <cafe.ina@outlook.com>
2023-03-29 19:14:16 +0000

    Reorganizing code.

--- Cliente_Chat.java
+++ Cliente_Chat.java
@@ -1,28 +1,54 @@
 import java.net.*;
-import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Scanner;
+import java.io.IOException;
 
 class Cliente_Chat
 {
 	public static void main(String[] args)
-			throws UnknownHostException , IOException
+			throws IOException, InterruptedException
 	{
 		String ip		= new String("127.0.0.1");
-		int porta		= 4381;
-		Socket cliente		= new Socket(ip, porta);
-        	Scanner teclado		= new Scanner(System.in);
-		PrintStream saida	= new PrintStream(cliente.getOutputStream());
+		int porta_server	= 1234;
+		Emissor i_output	= new Emissor(ip, porta_server);
 
 		System.out.println("App de Conversacoes");
 		System.out.println("______________________________________________");
-		System.out.println("Conectado com usuario " + cliente.getInetAddress().getHostAddress());
-
-		while(teclado.hasNextLine())
-			saida.println(teclado.nextLine());
-
-		teclado.close();
-		saida.close();
-		cliente.close();
+		i_output.start();
+		i_output.join();
+		System.out.println("______________________________________________");
+		System.out.println("Fim do Bate-papo!");
 	}
 }
+
+class Emissor extends Thread
+{
+	private Socket receptor;
+	
+	public Emissor(String ip, int porta)
+	{
+		try
+		{
+			this.receptor	= new Socket(ip, porta);
+		}
+		catch(Exception e) {}
+	}
+
+	public void run()
+	{
+		try
+		{
+			Scanner teclado		= new Scanner(System.in);
+			PrintStream saida	= new PrintStream(this.receptor.getOutputStream());
+
+			while(teclado.hasNextLine())
+				saida.println(teclado.nextLine());
+
+			teclado.close();
+			saida.close();
+			this.receptor.close();
+		}
+		catch(Exception e) {}
+	}
+}
+
--- Servidor_Chat.java
+++ Servidor_Chat.java
@@ -8,82 +8,77 @@ class Servidor_Chat
 	public static void main(String[] args)
 			throws IOException, InterruptedException
 	{
-		int porta			= 4381;
-		int max_espera_requisao		= 5;
-		ServerSocket servidor_chat	= new ServerSocket(porta);
-		Gestor_Conexoes gestao_chat	= new Gestor_Conexoes(servidor_chat);
+		int porta_server	= 1234;
+		int max_t_req		= 5;
+		ServerSocket servidor	= new ServerSocket(porta_server);
+		Adm_Conexoes i_input	= new Adm_Conexoes(servidor);
 
-		servidor_chat.setSoTimeout(max_espera_requisao * 1000);
+		servidor.setSoTimeout(max_t_req * 1000);
 		System.out.println("App de Conversacoes");
 		System.out.println("______________________________________________");
-		gestao_chat.start();
-		gestao_chat.join();
+		i_input.start();
+		i_input.join();
+		servidor.close();
 		System.out.println("______________________________________________");
 		System.out.println("Fim do Bate-papo!");
-
-		servidor_chat.close();
 	}
 }
 
-class Gestor_Conexoes extends Thread
+class Adm_Conexoes extends Thread
 {
 	private ServerSocket server;
 
-	public Gestor_Conexoes(ServerSocket server)
+	public Adm_Conexoes(ServerSocket server)
 	{
 		this.server	= server;
 	}
 
 	public void run()
 	{
-		while(Conexao.con_ativas() == 0)
+		while(Receptor.con_ativas() == 0)
 		{
 			try
 			{
-				new Conexao(this.server.accept());
+				new Receptor(this.server.accept());
 			}
 			catch (Exception e) {}
 		}
 
-		while(Conexao.con_ativas() != 0)
+		while(Receptor.con_ativas() != 0)
 		{
 			try
 			{
-					new Conexao(this.server.accept());
+				new Receptor(this.server.accept());
 			}
 			catch (Exception e) {}
 		}
 	}
 }
 
-class Conexao extends Thread
+class Receptor extends Thread
 {
 	private static int ativas = 0;
 	private Socket cliente;
 
-	public Conexao(Socket endereco_cliente)
+	public Receptor(Socket endereco_cliente)
 	{
 		this.cliente = endereco_cliente;
 		this.start();
-		Conexao.ativas++;
+		Receptor.ativas++;
 	}
 
 	public void run()
 	{
 		try
 		{
-			Scanner msg		= new Scanner(this.cliente.getInputStream());
-			String ip_cliente	= this.cliente.getInetAddress().getHostAddress();
-
-			System.out.println(ip_cliente + " se conectou.");
+			Scanner msg	= new Scanner(this.cliente.getInputStream());
 
 			while(msg.hasNextLine())
 				System.out.println(msg.nextLine());
 
-			System.out.println(ip_cliente + " se desconectou.");
 			msg.close();
 			this.cliente.close();
-			Conexao.ativas--;
+			Receptor.ativas--;
 		}
 		catch(IOException e)
 		{
@@ -93,6 +88,7 @@ class Conexao extends Thread
 
 	public static int con_ativas()
 	{
-		return Conexao.ativas;
+		return Receptor.ativas;
 	}
 }
+
