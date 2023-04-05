8cd107156f5f6a0c2a50c25b980601e7cbe7d187
Inaue Ferreira da Silva <inaue.silva@gmail.com>
2023-04-04 22:19:07 -0300

    Keeping it Simple + users can receive msgs.

--- Cliente_Chat.java
+++ Cliente_Chat.java
@@ -1,54 +1,64 @@
-import java.net.*;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Scanner;
-import java.io.IOException;
+import java.net.*;
 
 class Cliente_Chat
 {
-	public static void main(String[] args)
-			throws IOException, InterruptedException
-	{
-		String ip		= new String("127.0.0.1");
-		int porta_server	= 1234;
-		Emissor i_output	= new Emissor(ip, porta_server);
-
-		System.out.println("App de Conversacoes");
-		System.out.println("______________________________________________");
-		i_output.start();
-		i_output.join();
-		System.out.println("______________________________________________");
-		System.out.println("Fim do Bate-papo!");
-	}
+    public static void main(String[] args)
+        throws IOException, UnknownHostException, InterruptedException
+    {
+        Socket servidor     = new Socket("localhost", 1234);
+        Conexao chat        = new Conexao(servidor);
+        
+        chat.start();
+        chat.join();
+    }
 }
 
-class Emissor extends Thread
+class Conexao extends Thread
 {
-	private Socket receptor;
-	
-	public Emissor(String ip, int porta)
-	{
-		try
-		{
-			this.receptor	= new Socket(ip, porta);
-		}
-		catch(Exception e) {}
-	}
+    private Socket maquina;
+    private Transmissor fala;
+    private Transmissor escuta;
 
-	public void run()
-	{
-		try
-		{
-			Scanner teclado		= new Scanner(System.in);
-			PrintStream saida	= new PrintStream(this.receptor.getOutputStream());
+    public Conexao(Socket maquina)
+        throws IOException
+    {
+        this.escuta	= new Transmissor(new Scanner(maquina.getInputStream()), System.out);
+        this.fala	= new Transmissor(new Scanner(System.in), new PrintStream(maquina.getOutputStream()));
+    }
 
-			while(teclado.hasNextLine())
-				saida.println(teclado.nextLine());
+    public void run()
+    {
+        try
+        {
+            this.fala.start();
+            this.escuta.start();
+            this.fala.join();
+            this.escuta.join();
+        }
+        catch(Exception e) {}
+    }
+}
 
-			teclado.close();
-			saida.close();
-			this.receptor.close();
-		}
-		catch(Exception e) {}
-	}
+class Transmissor extends Thread {
+    private Scanner in;
+    private PrintStream out;
+
+    public Transmissor(Scanner in, PrintStream out)
+        throws IOException
+    {
+	    this.in	=	in;
+	    this.out	=	out;
+    }
+
+    public void run()
+    {
+        while (this.in.hasNextLine())
+            this.out.println(this.in.nextLine());
+
+        this.in.close();
+    }
 }
 
--- Servidor_Chat.java
+++ Servidor_Chat.java
@@ -1,94 +1,56 @@
-import java.net.*;
-import java.lang.Thread;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.net.*;
 
-class Servidor_Chat
+public class Servidor_Chat
 {
-	public static void main(String[] args)
-			throws IOException, InterruptedException
+	public static void main(String[] args) throws IOException
 	{
-		int porta_server	= 1234;
-		int max_t_req		= 5;
-		ServerSocket servidor	= new ServerSocket(porta_server);
-		Adm_Conexoes i_input	= new Adm_Conexoes(servidor);
+		Collection<PrintStream> saidas	= new HashSet<PrintStream>(); 
+		ServerSocket servidor		= new ServerSocket(1234);
+		int max_t_req			= 5;
+		Scanner entrada;
+		PrintStream saida;
+		Socket cliente;
 
-		servidor.setSoTimeout(max_t_req * 1000);
-		System.out.println("App de Conversacoes");
-		System.out.println("______________________________________________");
-		i_input.start();
-		i_input.join();
-		servidor.close();
-		System.out.println("______________________________________________");
-		System.out.println("Fim do Bate-papo!");
+		while (true)
+		{
+			cliente = servidor.accept();
+			entrada	= new Scanner(cliente.getInputStream());
+			saida	= new PrintStream(cliente.getOutputStream());
+			saidas.add(saida);
+			new Megafone(entrada, saidas).start();
+		}
 	}
 }
 
-class Adm_Conexoes extends Thread
+class Megafone extends Thread
 {
-	private ServerSocket server;
+	private Scanner in;
+	private Collection<PrintStream> out;
 
-	public Adm_Conexoes(ServerSocket server)
+	public Megafone(Scanner in, Collection<PrintStream> out)
+		throws IOException
 	{
-		this.server	= server;
+		this.in		= in;
+		this.out	= out;
 	}
 
 	public void run()
 	{
-		while(Receptor.con_ativas() == 0)
+		String msg;
+
+		while (this.in.hasNextLine())
 		{
-			try
-			{
-				new Receptor(this.server.accept());
-			}
-			catch (Exception e) {}
+			msg	= this.in.nextLine();
+
+			for(PrintStream cliente : this.out)
+				cliente.println(msg);
 		}
 
-		while(Receptor.con_ativas() != 0)
-		{
-			try
-			{
-				new Receptor(this.server.accept());
-			}
-			catch (Exception e) {}
-		}
+		this.in.close();
 	}
 }
-
-class Receptor extends Thread
-{
-	private static int ativas = 0;
-	private Socket cliente;
-
-	public Receptor(Socket endereco_cliente)
-	{
-		this.cliente = endereco_cliente;
-		this.start();
-		Receptor.ativas++;
-	}
-
-	public void run()
-	{
-		try
-		{
-			Scanner msg	= new Scanner(this.cliente.getInputStream());
-
-			while(msg.hasNextLine())
-				System.out.println(msg.nextLine());
-
-			msg.close();
-			this.cliente.close();
-			Receptor.ativas--;
-		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
-
-	public static int con_ativas()
-	{
-		return Receptor.ativas;
-	}
-}
-
